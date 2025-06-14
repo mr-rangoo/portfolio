af95d1ceee36129996f22eaf194c00a8a3f93e24
Rango Oj <oscarjacky4@gmail.com>
2025-06-14 21:18:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -47,6 +47,7 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.container, .about-content', { origin: 'right' });
 
 // Typed.js for typing effect
 const typed = new Typed('.multiple-text', {
